3a46fe8a496c04818d6f498df83c961914556452
Edson Lek Hong Ma <edsonma@gmail.com>
2018-04-04 11:01:16 -0300

    changed service port to generic env variable

--- iptables.sh
+++ iptables.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-iptables -t nat -A PREROUTING -p tcp -m tcp --dport 1434 -j DNAT --to-destination $SERVICE_IP:$SERVICE_PORT
-iptables -t nat -A POSTROUTING -p tcp -m tcp --dport 1434 -j MASQUERADE
+iptables -t nat -A PREROUTING -p tcp -m tcp --dport $SERVICE_PORT -j DNAT --to-destination $SERVICE_IP:$SERVICE_PORT
+iptables -t nat -A POSTROUTING -p tcp -m tcp --dport $SERVICE_PORT -j MASQUERADE
