36d22f1dc5b44b06db937bf004ae9c36cdf9a5b1
Luciano Prestes Cavalcanti <lucianopcbr@gmail.com>
2018-08-14 19:40:57 +0000

    Add iptables rules to redirect

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,12 @@
 echo "tuntap creation"
 ip tuntap add vpn0 mode tun user root
 
+echo "Adding redirect ports"
+echo $REDIRECT_PORTS | tr : '\n' | while read redirect_port; do
+    iptables -t nat -A PREROUTING -p tcp -m tcp --dport $redirect_port -j DNAT --to-destination $REDIRECT_IP:$redirect_port
+    iptables -t nat -A POSTROUTING -p tcp -m tcp --dport $redirect_port -j MASQUERADE
+done
+
 echo "Adding nat rules"
 iptables -t nat -A PREROUTING -p tcp -m tcp --dport $SERVICE_PORT -j DNAT --to-destination $SERVICE_IP:$SERVICE_PORT
 iptables -t nat -A POSTROUTING -p tcp -m tcp --dport $SERVICE_PORT -j MASQUERADE
